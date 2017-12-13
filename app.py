b67013ad28bc3cb229759e9314be6843b824831a
Apple <Apple@Apples-MacBook-Pro-2.local>
2017-12-13 14:23:21 +0530

    v2_test_1

--- app.py
+++ app.py
@@ -715,7 +715,7 @@ def processRequest(req):
     print("processing req")
     input_data = str(req)
     print(input_data)
-    input_data = str(req.get("result").get("resolvedQuery"))
+#    input_data = str(req.get("result").get("resolvedQuery"))
     input_data = str(req.get("queryResult").get("queryText"))
     input_data = re.sub('[sS]ong', '', input_data)
     input_data = re.sub('^[pP]lay', '', input_data)
