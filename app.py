9eaf65b9b28974637aaa586d47d9924899d2e88b
deepak02 <guptadeepak.iitd@gmail.com>
2017-12-16 22:47:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def webhook():
     session_id=str(req.get("sessionId"))
     print("Session id2",session_id)
     #next_req = q.enqueue(requests.post('https://drj1.herokuapp.com/next', data = {'session_id1':session_id}))
-    next_req = q.enqueue(next(session_id))
+    #next_req = q.enqueue(next(session_id))
     #result = q.enqueue(count_words_at_url, 'http://heroku.com')
     time.sleep(1)
     print("queue working")
