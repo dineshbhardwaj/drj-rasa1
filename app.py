600459c47c4c9053f075d45b683b47c5d6c5f2c5
deepak02 <guptadeepak.iitd@gmail.com>
2017-12-18 12:09:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,8 @@ def webhook():
     session_id=req.get("sessionId")
     print("Session id2",session_id)
     #next_req = q.enqueue(requests.post('https://drj1.herokuapp.com/next', data = {'session_id1':session_id}))
-    next_req = q.enqueue(next_event,session_id)
+    if str(req.get("result").get("source")) not contains "agent"
+        next_req = q.enqueue(next_event,session_id)
     #result = q.enqueue(count_words_at_url, 'http://heroku.com')
     #time.sleep(1)
     print("queue working")
