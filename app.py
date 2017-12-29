ff3417e9a0e7570cfa7c2c2429d02c05457db3e6
dineshbhardwaj <dinbhardwaj@gmail.com>
2017-12-29 12:57:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,34 +30,14 @@ import apiai
 import requests
 import time
 
-
-from flask import Flask
-from flask import request
-from flask import make_response
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
-from song_data import *
-from rq import Queue
-from worker import conn
-#from rasa_nlu_prog import *
-
 from utils import next_event
 
 
 maintain_history = []
 max_song_num=5
 
-
-
 # Flask app should start in global layout
 app = Flask(__name__)
-
-
-
-#DEEPAK CODE FOR EVENT
-CLIENT_ACCESS_TOKEN = '9ded4fd3df4b42b7b678b928add51dbf'
-##DEEPAK
-
 
 
 @app.route('/webhook', methods=['POST'])
@@ -68,75 +48,13 @@ def webhook():
     req = urlopen('http://ec2-54-197-17-247.compute-1.amazonaws.com').read()
     print("done req")
     #req = requests.post('http://ec2-54-197-17-247.compute-1.amazonaws.com')
-    req2=requests.post('http://google.com')
-    print("done req2")    
     print(req)
-    print(req2)
-    print("done webhook")
-#    req = request.get_json(silent=True, force=True)
-#    q = Queue(connection=conn)
-#    print("Request:")
-#    print(json.dumps(req, indent=4))
-#    res = processRequest(req)
-#    res = json.dumps(res, indent=4)
-    # print(res)
-#    r = make_response(res)
-#    r.headers['Content-Type'] = 'application/json'
-    #if not session_id:
-#    print("session id1")
-#    session_id=req.get("sessionId")
-#    print("Session id2",session_id)
-    #next_req = q.enqueue(requests.post('https://drj1.herokuapp.com/next', data = {'session_id1':session_id}))
-    #if "nextevent" not in str(req.get("result").get("resolvedQuery")):
-        #next_req = q.enqueue(next_event,session_id)
-    #result = q.enqueue(count_words_at_url, 'http://heroku.com')
-    #time.sleep(1)
-
-#    print("queue working")
-    return req
-
-
-
-#temp def processRequest(req):
-#temp     print("processing req")
-#temp     input_data = str(req)
-#temp     print(input_data)
-#temp     input_data = str(req.get("result").get("resolvedQuery"))
-#   input_data = str(req.get("queryResult").get("queryText"))
-#temp     choice_val_list = run_hindiSong(input_data)
-#temp     entity_name_list = choice_val_list[0]
-#temp     entity_val_list = choice_val_list[0]
-    
-#temp     print("processing req 3")
-#temp     if "song_name" in  entity_name_list[0]:
-#temp             choice_val = process.extract(entity_val_list[0], choices, scorer=fuzz.partial_ratio, limit=1)
-#temp     choice_song_path = map_choices[choice_val[0][0]]
-#temp     choice_song_path1 = map_choices["Tera Mera Pyar Amar"]
-#temp     print("processing req 4")
-#temp     print(str(choice_song_path))
-#temp     data = "<speak> <audio src=\"" + choice_song_path + "\"> didn't get your MP3 audio file </audio> </speak>"
-    
-    ###CODE FOR PLAYLIST####
-    ## temp removed song_num=0
-    ## temp removed playlist_songs_paths=[]
-    ## temp removed for key in map_choices:
-    ## temp removed     if (song_num < max_song_num):
-    ## temp removed         song_num = song_num+1
-    ## temp removed         playlist_songs_paths.append(map_choices[key])
-    ## temp removed     
-    ## temp removed data="<speak>"
-    ## temp removed for song_path in playlist_songs_paths:
-    ## temp removed     data = data + "<audio src=\"" + song_path + "\"> didn't get your MP3 audio file </audio> <break time=\"2s\"/> "
-    ## temp removed data= data + "</speak>"
-    ############################################
-    
-    ##CODE FOR SINGLE SONG###########################################
-    #data = "<speak> <audio src=\"" + choice_song_path + "\"> didn't get your MP3 audio file </audio> \
-    #    <break time=\"3s\"/> \
-    #    <audio src=\"" + choice_song_path1 + "\"> didn't get your MP3 audio file </audio> </speak>"
-    ########################################
-#temp     res = makeWebhookResult(data)
-#temp     return res
+    res = json.dumps(req, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    print("queue working")
+    return r
 
 
 def makeWebhookResult(data):
